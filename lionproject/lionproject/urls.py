285bf491e7ece13571c6118c18d5f0c220b8a4aa
Seoinguk-1023 <kojkoj95@naver.com>
2021-06-08 10:49:41 +0900

    6.8 CRUD delete 강의 수강 후 적용

--- lionproject/lionproject/urls.py
+++ lionproject/lionproject/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('create/',create,name="create"),
     path('edit/<str:id>',edit,name="edit"),
     path('update/<str:id>',update,name="update"),
+    path('delete/<str:id>',delete, name = "delete"),
     
 ]   
