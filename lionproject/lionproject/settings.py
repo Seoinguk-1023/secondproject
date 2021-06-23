f0fc2bcd28abb5a671d6aa7b911776c91e17f2df
Seoinguk-1023 <kojkoj95@naver.com>
2021-06-23 10:01:45 +0900

    6.23 media 강의 수강

--- lionproject/lionproject/settings.py
+++ lionproject/lionproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,7 +120,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_DIRS = [
+    os.path.join(BASE_DIR,'blog','static') ]
+    #현재 스태틱파일이 저장된곳
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#스태틱파일을 어디에 모을건지
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#이용자가 업로드한 파일을 모으는 곳
+MEDIA_URL = '/media/'
